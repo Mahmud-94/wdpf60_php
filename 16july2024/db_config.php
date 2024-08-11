11180139b114b8c789273e5b810cda9d461c4582
Mahmud-94 <mahamudulhassanredoyQgmail.com>
2024-08-11 13:02:31 +0600

    10 august

---
+++ 16july2024/db_config.php
@@ -0,0 +1,45 @@
+<?php 
+
+  $host ="localhost";
+  $user = "root";
+  $pass = "";
+  $db = "wdpf60";
+
+$con = new mysqli($host, $user, $pass, $db);
+
+if ($con->connect_error) {
+    echo "Failed to connect to Mysql: " . $con->connect_error;
+    exit();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is home page</h1>
+
+    <?php 
+
+
+$data = $con->query("SELECT * FROM users");
+
+
+
+// $output = $data->fetch_array();
+// echo "<pre>";
+// print_r($data);
+
+$output = $data->fetch_assoc();
+echo "<pre>";
+print_r($output);
+    ?>
+   
+</body>
+</html>
