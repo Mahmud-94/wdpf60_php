82f3f8049dfebfd04527c5dda4003eeff779a2aa
Siyam115 <siyamhasan115@gmail.com>
2024-09-02 13:25:42 +0600

    2 september

---
+++ 2september2024/app/db_config.php
@@ -0,0 +1,13 @@
+<?php $db = new mysqli("localhost", "root", "", "wdpf_60"); 
+if($db->connect_errno){
+    echo $db->connect_error;
+    exit();
+}
+
+
+?>
+
+
+    
+</body>
+</html>
---
+++ 2september2024/app/login.php
@@ -0,0 +1,26 @@
+<?php
+include_once("db_config.php");
+
+session_start();
+
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+$password= md5($password);
+
+    $result = $db->query("SELECT * FROM users WHERE email = '$email' AND password ='$password'");
+    $row = $result->fetch_assoc();
+
+    if($result->num_rows==0){
+        $_SESSION['login_err']= "Email or password may wrong, pls try again";
+
+    header("Location: index.php");
+    }else{
+        $_SESSION['email']= $email;
+        $_SESSION['name']= $row['fullname'];
+
+        header("Location: dashboard.php");
+    }
+}
+?>
