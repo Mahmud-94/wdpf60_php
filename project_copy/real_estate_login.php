a99de1952bf9ded73553ebe7ae83e5d9218ff573
Siyam115 <siyamhasan115@gmail.com>
2024-09-09 13:46:30 +0600

    9 sep

---
+++ project_copy/real_estate_login.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Seo meta tag -->
+    <meta name="author" content="mamun2074">
+    <meta name="keywords"
+        content="account, Accounting Software, balance sheet, cash flow, Cost Of Revenue, fixed asset schedule, ledger, multi branch, Profit Or Loss Account, receive payment, trial balance">
+    <meta name="description"
+        content="E-Account is a dynamic, open source, easy to use, user friendly web base application. Which is built in PHP – MySQL">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- End -->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="miNrKzLM9xTIWsPD5vupOxVaLjYiOOqxC3HlEIDt">
+    <title>Welcome To | systeme Private Limited</title>
+    <link rel="icon" href="https://demo.erealestate.eaccount.xyz/upload/company-logo/1721644779Innovax.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css"
+        href="https://demo.erealestate.eaccount.xyz/asset/layout/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://demo.erealestate.eaccount.xyz/asset/layout/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://demo.erealestate.eaccount.xyz/asset/css/e-bootstrap.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="https://demo.erealestate.eaccount.xyz/asset/layout/css/util.css">
+    <link rel="stylesheet" type="text/css" href="https://demo.erealestate.eaccount.xyz/asset/layout/css/main.css">
+</head>
+
+<body>
+    <div>
+            <div class="limiter">
+        <div class="container-login100" style="background-image: url( 'https://demo.erealestate.eaccount.xyz/asset/layout/images/banner.jpg');">
+            <div class="wrap-login100">
+                <form id="sign_in" method="POST" action="https://demo.erealestate.eaccount.xyz/login" class="login100-form validate-form">
+                    <input type="hidden" name="_token" value="miNrKzLM9xTIWsPD5vupOxVaLjYiOOqxC3HlEIDt">                    <span class="login100-form-logo">
+                        <img class="w-50 h-50" src="https://demo.erealestate.eaccount.xyz/upload/company-logo/1721644779Innovax.png">
+                    </span>
+                    <span class="login100-form-title p-b-34 p-t-20">
+                        Log in
+                    </span>
+                    <div class="wrap-input100 validate-input" data-validate="E-Mail Address">
+                        <input placeholder="E-Mali Address" id="email" type="email" class="input100 " name="email"
+                            value="">
+                        <span class="focus-input100" data-placeholder="&#xf15a;"></span>
+                                            </div>
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input placeholder="Password" id="password" type="password" class="input100" name="password">
+                        <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                                            </div>
+                    <div class="contact100-form-checkbox">
+                        <input id="ckb1" class="input-checkbox100" type="checkbox" name="remember" id="remember"
+                            >
+                        <label class="label-checkbox100" for="ckb1">
+                            Remember Me
+                        </label>
+                    </div>
+                    <div class="container-login100-form-btn">
+                        <button type="submit" class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+                                            <div class="text-center p-t-30">
+                            <table class="table table-bordered table-sm table-hover table-info">
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Demo Login</th>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Password</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>admin@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                    <tr>
+                                        <td>accountant@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                    <tr>
+                                        <td>projectmanager@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                    <tr>
+                                        <td>productmanager@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sellsmanager@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                    <tr>
+                                        <td>purchasemanager@eaccount.xyz</td>
+                                        <td>1234</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                                    </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Jquery Core Js -->
+    <script src="https://demo.erealestate.eaccount.xyz/asset/layout/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="https://demo.erealestate.eaccount.xyz/asset/layout/js/main.js"></script>
+</body>
+
+</html>
