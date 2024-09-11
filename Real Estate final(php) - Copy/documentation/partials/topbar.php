439eef733ce09a0faaa2a098b02be67baca1f204
Siyam115 <siyamhasan115@gmail.com>
2024-09-11 13:23:25 +0600

    11 sep

---
+++ Real Estate final(php) - Copy/documentation/partials/topbar.php
@@ -0,0 +1,44 @@
+<div class="navbar-area navbar-area-3" style=" height: 200px;">
+        <nav class="navbar navbar-expand-lg" style=" height: 200px;">
+            <div class="container nav-container">
+                <div class="responsive-mobile-menu">
+                    <button class="menu toggle-btn d-block d-lg-none" data-target="#dkt_main_menu" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="icon-left"></span>
+                        <span class="icon-right"></span>
+                    </button>
+                </div>
+                
+                <div class="nav-right-part nav-right-part-mobile">
+                    <a class="btn btn-base" href="add-property.php">Submit</a>
+                </div>
+                <div class="collapse navbar-collapse" id="dkt_main_menu">
+                    <ul class="navbar-nav menu-open text-center">
+                        <li>
+                            <a href="index.php">Home</a>
+
+                        </li>
+                        <li>
+                            <a href="property-left-sidebar.php">Property</a>
+
+                        </li>
+                        <li>
+                            <a href="about.php">About</a>
+
+                        </li>
+                        <li>
+                            <a href="blog-grid.php">Blog</a>
+
+                        </li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="nav-right-part nav-right-part-desktop">
+                    <ul>
+                        <li><a class="search" href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a class="btn btn-base" href="add-property.php">Submit <i class="fa fa-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
